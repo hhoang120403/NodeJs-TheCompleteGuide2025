85a7934bd3ed56732a8b540b79332da551593e60
Huy Hoang <hhoang12042003@gmail.com>
2025-06-26 21:17:49 +0700

    S6_Assignment

---
+++ S6_Assignment/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+const users = [];
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res, next) => {
+  res.render('index', { pageTitle: 'Add User' });
+});
+
+app.get('/users', (req, res, next) => {
+  res.render('users', {
+    pageTitle: 'User',
+    users: users,
+  });
+});
+
+app.post('/add-user', (req, res, next) => {
+  users.push({ name: req.body.username });
+  res.redirect('/users');
+});
+
+app.listen(3000);
