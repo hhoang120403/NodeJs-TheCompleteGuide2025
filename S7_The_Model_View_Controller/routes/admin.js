6584b25f9f36b70f229533e1fb151916198b965d
hhoang_1204 <hhoang12042003@gmail.com>
2025-08-05 10:24:38 +0700

    S23_Understanding_Async_Requests

--- S7_The_Model_View_Controller/routes/admin.js
+++ S7_The_Model_View_Controller/routes/admin.js
@@ -7,7 +7,7 @@ const {
   getProducts,
   getEditProduct,
   postEditProduct,
-  postDeleteProduct,
+  deleteProduct,
 } = require('../controllers/admin');
 
 const isAuth = require('../middleware/is-auth');
@@ -46,6 +46,6 @@ router.post(
   postEditProduct
 );
 
-router.post('/delete-product', isAuth, postDeleteProduct);
+router.delete('/product/:productId', isAuth, deleteProduct);
 
 module.exports = router;
