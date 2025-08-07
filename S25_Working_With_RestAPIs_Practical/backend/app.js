84339a7241ffafeb4f29e9d1389720e6808e3e03
hhoang_1204 <hhoang12042003@gmail.com>
2025-08-07 16:39:15 +0700

    S25_Working_With_RestAPIs_Practical

---
+++ S25_Working_With_RestAPIs_Practical/backend/app.js
@@ -0,0 +1,77 @@
+require('dotenv').config();
+
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, uuidv4() + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(express.json()); // parse incoming json data => application/json
+app.use(multer({ storage: fileStorage, fileFilter }).single('image'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+/**
+ * If you run sever and client on 2 different domains => CORS eror
+ * => res.setHeader('Access-Control-Allow-Origin', '*');
+ * => res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+ * => res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+ */
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  const status = err.statusCode || 500;
+  const message = err.message;
+  const data = err.data;
+  res.status(status).json({
+    message,
+    data,
+  });
+});
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.tuxfxmj.mongodb.net/postdb?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then((result) => app.listen(8080))
+  .catch((err) => console.log(err));
